a1fdae2f4bd6d73ad884654d935d268a185fdbf1
wasim <wqab0402@g.educaand.es>
2026-01-12 13:34:16 +0100

    he hecho el repaso 3

---
+++ repaso3.py
@@ -0,0 +1,18 @@
+a s= float(int("pon un número")
+if a > 0:
+    print("es número positivo")
+elif a == 0:
+    print("el número es 0")
+else
+print("el número es negativo")
+
+
+
+
+
+
+a = int(input("pon un año"))
+if (a % 4 == 0) and (a % 100 !=) or (a & 400):
+    print("Es un año bisiesto")
+else:
+    pritn("Error")
